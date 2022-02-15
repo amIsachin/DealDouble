5eecafc982f91de2f9324b990316d1855fa01ef5
sachin <amIsachin@hotmail.com>
2022-02-15 13:01:54 +0530

    Add project files.

---
+++ DeadDouble.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeadDouble.Web.Startup))]
+namespace DeadDouble.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
