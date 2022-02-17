f44ddb7e9eba9de1e90a07002c9097772236ce49
sachin <amIsachin@hotmail.com>
2022-02-17 15:00:24 +0530

    Complete Crud With jQuery ajax

--- DealDouble.Services/AuctionsService.cs
+++ DealDouble.Services/AuctionsService.cs
@@ -1,5 +1,7 @@
 ﻿using DealDouble.Data;
 using DealDouble.Entities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DealDouble.Services
 {
@@ -10,6 +12,40 @@ namespace DealDouble.Services
             using (var context = new DealDoubleContext())
             {
                 context.Auctions.Add(auction);
+                context.SaveChanges();
+            }
+        }
+
+        public Auction GetAuctionByID(int ID)
+        {
+            using (var context = new DealDoubleContext())
+            {
+                return context.Auctions.Find(ID);
+            }
+        }
+
+        public void UpdateAuction(Auction auction)
+        {
+            using (var context = new DealDoubleContext())
+            {
+                context.Entry(auction).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Auction> GetAllAuction()
+        {
+            using (var context = new DealDoubleContext())
+            {
+                return context.Auctions.ToList();
+            }
+        }
+
+        public void DeleteAuction(Auction auction)
+        {
+            using (var context = new DealDoubleContext())
+            {
+                context.Entry(auction).State = System.Data.Entity.EntityState.Deleted;
                 context.SaveChanges();
             }
         }
--- DealDouble.Web/Controllers/AuctionsController.cs
+++ DealDouble.Web/Controllers/AuctionsController.cs
@@ -8,17 +8,57 @@ namespace DealDouble.Web.Controllers
     {
         AuctionsService service = new AuctionsService();
 
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(service.GetAllAuction());
+            }
+            else
+            {
+                return View(service.GetAllAuction());
+            }
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            return PartialView();
         }
 
         [HttpPost]
         public ActionResult Create(Auction auction)
         {
             service.SaveAuction(auction);
-            return View();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var Auction = service.GetAuctionByID(ID);
+            return PartialView(Auction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            service.UpdateAuction(auction);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            var Auction = service.GetAuctionByID(ID);
+            return View(Auction);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            service.DeleteAuction(auction);
+            return RedirectToAction("Index");
         }
     }
 }
