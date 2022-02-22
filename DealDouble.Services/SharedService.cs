0d694660c8e12d5c54f0a7cb19d963b812e582a8
amIsachin <amIsachin@hotmail.com>
2022-02-22 14:53:39 +0530

    Create Shared Controller And Shared Service to manipulate Images But Image work not properly done

---
+++ DealDouble.Services/SharedService.cs
@@ -0,0 +1,18 @@
+﻿using DealDouble.Data;
+using DealDouble.Entities;
+
+namespace DealDouble.Services
+{
+    public class SharedService
+    {
+        public int SavePicture(Picture picture)
+        {
+            using (var context = new DealDoubleContext())
+            {
+                context.Pictures.Add(picture);
+                context.SaveChanges();
+                return picture.ID;
+            }
+        }
+    }
+}
--- DealDouble.Web/Controllers/SharedController.cs
+++ DealDouble.Web/Controllers/SharedController.cs
@@ -1,12 +1,39 @@
-﻿using System.Web.Mvc;
+﻿using DealDouble.Entities;
+using DealDouble.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
 
 namespace DealDouble.Web.Controllers
 {
     public class SharedController : Controller
     {
-        public ActionResult UploadPitures()
+        SharedService sharedService = new SharedService();
+
+        [HttpPost]
+        public JsonResult UploadPitures()
         {
-            return View();
+            JsonResult result = new JsonResult();
+            List<object> picturesJSON = new List<object>();
+
+            var pictures = Request.Files;
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                var picture = pictures[i];
+                var fileName = Guid.NewGuid() + Path.GetExtension(picture.FileName);
+                var path = Server.MapPath("~/Content/ThemeMaterial/Images/") + fileName;
+                picture.SaveAs(path);
+
+                var dbPicture = new Picture();
+                dbPicture.URL = path;
+                var pictureID = sharedService.SavePicture(dbPicture);
+
+                picturesJSON.Add(new { ID = pictureID, pictureURL = fileName });
+            }
+
+            result.Data = picturesJSON;
+            return result;
         }
     }
 }
